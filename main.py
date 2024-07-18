7fdee20a605dccf1b5fc376586dc80612cb71313
lukesj07 <lukesjennings07@gmail.com>
2024-07-17 21:05:24 -0400

    doesn't work

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import openpyxl
 import pandas as pd
 from datetime import date
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
+import string
+import numpy as np
+
+from openpyxl.styles import colors
+from openpyxl.styles import Font, Color
 
 # https://stackoverflow.com/questions/50172905/center-a-label-inside-a-circle-with-matplotlib
 
@@ -15,6 +21,108 @@ def parse_date(d: str) -> date:
     splt = map(int, d.split("-"))
     return date(splt[0], splt[1], splt[2])
 
+def create_radar_ids() -> list[str]:
+    ids = []
+    for i in range(100):
+        if i >= 10:
+            ids.append(str(i))
+        else:
+            ids.append("0" + str(i))
+
+    for c in string.ascii_uppercase:
+        for i in range(10):
+            ids.append(c + str(i))
+    
+    return ids
+
+def sort_project_status(df: pd.DataFrame) -> list[list[str]]:
+    green = []
+    amber = []
+    hold = []
+    red = []
+    for index, row in df.iterrows():
+        match row["Overall Health"]:
+            case "GREEN":
+                green.append(row["Project Name"])
+            case "Amber":
+                amber.append(row["Project Name"])
+            case "ON HOLD":
+                hold.append(row["Project Name"])
+            case "RED": # check
+                red.append(row["Project Name"])
+    return [green, amber, hold, red]
+
+def generate_excel(radar_ids: list[str], names: list[list[str]]) -> None:
+    gkey = "Green Status: " + str(len(names[0])) + " projects"
+    akey = "Amber Status: " + str(len(names[1])) + " projects"
+    okey = "On Hold: " + str(len(names[2])) + " projects"
+    rkey = "Red Status: " + str(len(names[3])) + " projects"
+
+    data = dict()
+    data[gkey] = []
+    data[akey] = []
+    data[okey] = []
+    data[rkey] = []
+    
+    idx = 0
+    for i in range(len(names)):
+        for j in range(len(names[i])):
+            match i:
+                case 0:
+                    data[gkey].append(names[i][j] + ": " + radar_ids[idx])
+                case 1:
+                    data[akey].append(names[i][j] + ": " + radar_ids[idx])
+                case 2:
+                    data[okey].append(names[i][j] + ": " + radar_ids[idx])
+                case 3:
+                    data[rkey].append(names[i][j] + ": " + radar_ids[idx])
+            idx += 1
+
+    max_len = max(map(len, data.values()))
+    for v in data.values():
+        for i in range(max_len-len(v)):
+            v.append(np.nan)
+
+    df = pd.DataFrame.from_dict(data)
+    print(df.head)
+    """
+    df.style.apply(lambda x: "color: green", subset=df[gkey])
+    df.style.apply(lambda x: "color: orange", subset=df[akey])
+    df.style.apply(lambda x: "color: gray", subset=df[okey])
+    df.style.apply(lambda x: "color: red", subset=df[rkey])
+    """
+
+    def color(val: str) -> str:
+        if val in df[gkey]:
+            return "color: green"
+        if val in df[akey]:
+            return "color: orange"
+        if val in df[okey]:
+            return "color: gray"
+        if val in df[rkey]:
+            return "color: red"
+
+    df.insert(1,"","")
+    df.insert(3," ","")
+    df.insert(5,"  ","")
+
+    df.to_excel("test.xlsx", index=False)
+    wb = openpyxl.load_workbook("test.xlsx")
+    ws = wb.active
+    for c in ["A", "C", "E", "G"]:
+        for i in range(1, max_len+1):
+            match c:
+                case "A":
+                    ws[c + str(i)].font = Font(color=colors.RGB())
+                case "C":
+                    ws[c + str(i)].font = Font(color=colors.ORANGE)
+                case "E":
+                    ws[c + str(i)].font = Font(color=colors.GRAY)
+                case "G":
+                    ws[c + str(i)].font = Font(color=colors.RED)
+
+    wb.save()
+    
 def main() -> None:
     df = pd.read_excel(DATA_PATH)
     df = df.dropna(how="all", axis=1)
@@ -25,7 +133,10 @@ def main() -> None:
     df = df.drop("index", axis=1)
 
     plt.imshow(mpimg.imread(IMG_PATH))
-    plt.show()
+    #plt.show()
+    generate_excel(create_radar_ids(), sort_project_status(df))
+    
+
 
 if __name__ == "__main__":
     main()
